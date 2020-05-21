4a5087bb9b0ef4c1042f2b7f18193a2f7bbdf3b4
Timofey-K <timca1997@mail.ru>
2020-05-21 19:18:02 +0300

    Events V1

---
+++ Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace events
+{
+    public delegate void PersonCameEventHandler(Person person, DateTime time);
+    public delegate void PersonLeaveEventHandler(Person person);
+    public class Person
+    {
+        public string Name { get; set; }
+
+
+        public void SayHello(Person person, DateTime time)
+        {
+            string SayTime;
+            if (time.Hour < 12)
+            {
+                SayTime = "Доброе утро";
+            }
+            else if (time.Hour > 17)
+            {
+                SayTime = "Добрый вечер";
+            }
+            else
+            {
+                SayTime = "Добрый день";
+            }
+
+            Console.WriteLine($"{SayTime}, {person.Name}! - Сказал {Name}");
+        }
+
+        public void SayBay(Person person)
+        {
+            Console.WriteLine($"Пока - {person.Name}! - Сказал {Name} ");
+        }
+
+
+        public event PersonCameEventHandler OnCome;
+        public event PersonLeaveEventHandler OnLeave;
+
+        public void GoToOfice()
+        {
+            OnCome?.Invoke(this, DateTime.Now);
+        }
+        public void GoHome()
+        {
+            OnLeave?.Invoke(this);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace events
+{
+	class Program
+	{
+
+		private static PersonCameEventHandler sayHelloDeligate;
+		private static PersonLeaveEventHandler sayLeaveDeligate;
+
+		static void Main(string[] args)
+		{
+			var rand = new Random();
+			List<Person> persons = new List<Person>()
+			{
+				new Person() { Name = "Аня" },
+				new Person() { Name = "Боря" },
+				new Person() { Name = "Вася" },
+				new Person() { Name = "Гриша" },
+				new Person() { Name = "Дина" },
+				new Person() { Name = "Ева" }
+			};
+
+			foreach (Person person in persons)
+			{
+				person.OnCome += Person_OnCome;
+				person.OnLeave += Person_OnLeave;
+				sayLeaveDeligate += person.SayBay;		
+			}
+
+			foreach (Person person in persons)
+			{				
+				person.GoToOfice();
+				sayHelloDeligate += person.SayHello;
+			}
+
+			foreach (Person person in persons)
+			{
+				
+				sayLeaveDeligate -= person.SayBay;
+				person.GoHome();
+
+				sayHelloDeligate -= person.SayHello;
+				person.OnCome -= Person_OnCome;				
+				person.OnLeave -= Person_OnLeave;
+			}				
+			Console.ReadKey();
+		}
+
+		private static void Person_OnCome(Person person, DateTime time)
+		{
+			Console.WriteLine($"[Пришёл работник {person.Name} ]");
+			sayHelloDeligate?.Invoke(person, time);
+		}
+
+		private static void Person_OnLeave(Person person)
+		{
+			Console.WriteLine($"[Ушёл работник {person.Name} ]");
+			sayLeaveDeligate?.Invoke(person);
+		}
+	}
+}
+
